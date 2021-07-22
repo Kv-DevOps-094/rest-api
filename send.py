fe29ceb96ea67d00044d3ffcef66c766b27ba23d
kimachinskiy <fedor.kimachinskiy@gmail.com>
2021-07-22 20:42:12 +0300

    Add pika send

---
+++ send.py
@@ -0,0 +1,16 @@
+import pika
+
+RABBIT_HOST = 'localhost'
+QUEUE = 'hello'
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST))
+channel = connection.channel()
+
+channel.queue_delete(queue=QUEUE)
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello world!')
+print("[x] Sent 'Hello world!'")
+
+connection.close()
