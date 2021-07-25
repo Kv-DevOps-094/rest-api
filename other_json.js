c0e62a4da7326ae494a24aa999ecca86dd47ae9d
kimachinskiy <48508661+kimachinskiy@users.noreply.github.com>
2021-07-25 14:08:00 +0300

    Add test data
    Add test data from rebbitmq to api

---
+++ other_json.js
@@ -0,0 +1,13 @@
+{
+    "action": "labeled",
+    "issue": {
+        "id": 947957902,
+        "state": "open",
+        "user": {
+            "login": "Mary1509",
+            "html_url": "https://github.com/Mary1509",
+            "avatar_url": "https://avatars.githubusercontent.com/u/54399719?v=4"
+        },
+        "data": "2021-07-19T19:26:36Z"
+    }
+}
